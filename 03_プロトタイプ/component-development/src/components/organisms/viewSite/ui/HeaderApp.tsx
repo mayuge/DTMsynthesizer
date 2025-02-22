4c30bfe118e0284d720b66e3934c57f73b300de1
mayuge <kei.tomo.kimi.ken@gmail.com>
2025-02-22 23:26:51 +0900

    feat:依存関係の整理

--- 03_プロトタイプ/component-development/src/components/organisms/viewSite/ui/HeaderApp.tsx
+++ 03_プロトタイプ/component-development/src/components/organisms/viewSite/ui/HeaderApp.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/atoms/buttons/Button"
 import SearchInput from "@/components/molecules/forms/SearchInput"
 import useViewSiteMain from "@/components/organisms/viewSite/core/application/useViewSiteMain"
 import LampCard from "@/components/molecules/frames/LampCard"
-import AccountCard from "@/components/molecules/frames/AccountCard"
+
 const HeaderApp: React.FC = () => {
   const { getModeColor, getModeText, getModeIcon, routeToHomeSite } = useViewSiteMain()
 
@@ -13,31 +13,17 @@ const HeaderApp: React.FC = () => {
     <div className="relative items-center bg-back w-full">
       <div className="flex items-center justify-between shadow-black shadow-sm gap-2">
         <Header
-          text="Relaha"
+          text="Raku-Raku-SynthMusic"
           icon="stat_minus_1"
           size="large"
           variant="header-dark"
           isCircle={false}
           onClick={routeToHomeSite}
         />
-        <div className="w-[250px]">
-          <AccountCard logoImg="" text="野澤遼太郎" infoButtonClick={() => {}} />
-        </div>
       </div>
 
       <div className="flex items-center justify-between bg-back shadow-black shadow-sm pr-4">
         <div className="flex items-center gap-11">
-          <div className="w-[350px]">
-            <SearchInput
-              placeholder="ユーザーを検索"
-              size="large"
-              shape="square"
-              onChange={() => {}}
-              onClick={() => {}}
-              isStretch={true}
-              isBlack={false}
-            />
-          </div>
           <LampCard
             text={`${getModeText()}`}
             variant={`${getModeColor()}`}
--- 03_プロトタイプ/component-development/src/components/organisms/viewSite/ui/ViewSiteMain.tsx
+++ 03_プロトタイプ/component-development/src/components/organisms/viewSite/ui/ViewSiteMain.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from "react"
+import React from "react"
 import AccountListBarApp from "@/components/organisms/viewSite/ui/AccountListBarApp"
 import RelationApp from "@/components/organisms/viewSite/ui/RelationApp"
 import SwitchApp from "@/components/organisms/viewSite/ui/SwitchApp"
@@ -9,25 +9,14 @@ import ModeDialogApp from "@/components/organisms/viewSite/ui/ProjectListApp"
 import ProfileApp from "@/components/organisms/viewSite/ui/ProfileApp"
 
 const ViewSiteMain: React.FC = () => {
-  const { useCallback } = useViewSiteMain()
-  useEffect(() => {
-    useCallback()
-  }, [])
-  //1分ごとに情報更新
-  useEffect(() => {
-    const interval = setInterval(() => {
-      useCallback()
-    }, 60000) // 1分（60秒）ごとに実行
+  const { } = useViewSiteMain()
 
-    return () => clearInterval(interval)
-  }, [useCallback])
 
   return (
     <div className="relative w-full h-screen">
       <HeaderApp />
       <div className="flex bg-gray-20 h-calc-100vh">
         <SwitchApp />
-        <AccountListBarApp />
         <div className="w-full overflow-y-auto no-scrollbar">
           <RelationApp />
           <div className="flex flex-wrap justify-center">
